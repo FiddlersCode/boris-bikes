59081e7150524fc1734e84af091db2135822b8df
Paula <pmuldoon@umich.edu>
2017-04-13 12:44:17 +0100

    organised tests in step13 by method calls

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,7 @@ describe DockingStation do
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
+      bike
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -11,20 +12,24 @@ describe DockingStation do
       subject.bike = nil
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
+
+    it 'releases working bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to be_working
+    end
+
+    it 'responds to release_bike' do
+      expect(subject).to respond_to :release_bike
+    end
   end
 
-  it 'responds to release_bike' do
-    expect(subject).to respond_to :release_bike
-  end
-  it 'releases working bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to be_working
-  end
-  it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike) }
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+  describe '#dock'do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it { is_expected.to respond_to(:bike) }
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
   end
 end
