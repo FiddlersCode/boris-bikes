33662ec9d233d458ad5855f0954dbd90f3bce571
Paula <pmuldoon@umich.edu>
2017-04-14 11:47:42 +0100

    red capacity method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -38,4 +38,11 @@ describe DockingStation do
       expect { subject.dock bike}.to raise_error 'Docking station full'
     end
   end
+
+  describe '#capacity' do
+    it 'has a default capacity' do
+      expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+    end
+  end
+
 end
